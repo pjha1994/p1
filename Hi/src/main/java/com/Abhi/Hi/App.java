6565f65d6fa6651384a37614b2385fa27a958706
pjha1994 <pjha1994@rediffmail.com>
2017-07-27 23:56:18 +0530

    1st commit files

---
+++ Hi/src/main/java/com/Abhi/Hi/App.java
@@ -0,0 +1,13 @@
+package com.Abhi.Hi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
